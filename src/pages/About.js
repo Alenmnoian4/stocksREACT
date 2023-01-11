fc10f524a3d34334e967f364365e5bfe51fbac10
Alenmnoian4 <alenmnoian4@gmail.com>
2023-01-10 20:57:37 -0500

    commit

---
+++ src/pages/About.js
@@ -0,0 +1,8 @@
+const About = (props) => {
+    return <> 
+    <h1>About Us</h1> 
+    <p>This is an application where you can view different stocks that are built on react.</p>
+    </>
+}
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,5 @@
+const Home = (props) => {
+    return <h1>Welcome to the Stock App Homepage</h1>
+}
+
+export default Home;
